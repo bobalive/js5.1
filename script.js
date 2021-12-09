647b3eb798f658a9a2aa92785da19ab7030c09c1
bob <bob@gmail.com>
2021-12-09 22:29:04 +0500

    js5.1

---
+++ script.js
@@ -0,0 +1,15 @@
+let num =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+
+console.log('Нечетные числа');
+for(let i = 0 ; i< num.length; i++){
+    console.log(num[i]);
+    i = i + 1
+}
+console.log('');
+console.log('четные числа')
+    for(let i = 1 ; i< num.length; i++){
+        console.log(num[i]);
+        i = i + 1
+    }
+
